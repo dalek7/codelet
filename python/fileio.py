9c980774383931df41a16e11394103c26c90a28e
Seung@MBP linc <ncraig@naver.com>
2018-02-20 12:22:46 +0900

    added float type

--- python/fileio.py
+++ python/fileio.py
@@ -8,3 +8,12 @@ b = np.loadtxt('test1.txt', dtype=int)
 print(a)
 print(b)
 print(a == b)
+
+
+a = np.array([1.1, 2.5, 3.0, 4])
+np.savetxt('test1.txt', a, fmt='%f')
+b = np.loadtxt('test1.txt', dtype=float)
+
+print(a)
+print(b)
+print(a == b)
