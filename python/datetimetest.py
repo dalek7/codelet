2ae65e23a3a15b22fe65c56d98fcbd772cf30c79
Seung@MBP linc <ncraig@naver.com>
2018-03-01 16:14:44 +0900

    datetime

---
+++ python/datetimetest.py
@@ -0,0 +1,14 @@
+
+import datetime
+
+s1 = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(s1)
+
+s2 = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+print(s2)
+
+
+'''
+2018-03-01 16:13:58
+20180301_161358
+'''
